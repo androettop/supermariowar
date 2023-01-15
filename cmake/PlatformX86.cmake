bf01fd0891e3296faa7bb87765f688e3080611b3
Pablo Androetto <androettop@gmail.com>
2023-01-15 14:07:24 -0300

    Update PlatformX86.cmake

--- cmake/PlatformX86.cmake
+++ cmake/PlatformX86.cmake
@@ -1,5 +1,5 @@
 # x86 specific compiler settings
 # ------------------------------
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse2")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -msse2")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse2 -arch x86_64")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -msse2 -arch x86_64")
